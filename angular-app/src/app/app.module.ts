5d8584482c98a0b243db23d94325edd26a929feb
Juan Martin Alvarez Colombo <85197820+alvarezcolombo95@users.noreply.github.com>
2023-10-30 23:13:10 -0300

    Generar componente navbar

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
