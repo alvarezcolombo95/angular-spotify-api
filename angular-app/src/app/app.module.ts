d1c9d0afbd3acd311af5c3f787d5ad3436302248
JoelArraigada <arraigadajoel@gmail.com>
2023-11-03 14:51:51 -0300

    Routes y inicio log in

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { LandingComponent } from './landing/landing.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AboutComponent } from './about/about.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LogInComponent } from './log-in/log-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
     NavbarComponent,
-    AboutComponent
+    AboutComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
