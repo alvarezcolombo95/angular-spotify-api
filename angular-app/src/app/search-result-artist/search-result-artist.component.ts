3f4a28c36ab2e834c9dc16b8dbea5c06ad31e194
Juan Martin Alvarez Colombo <85197820+alvarezcolombo95@users.noreply.github.com>
2023-11-05 12:46:20 -0300

    Agregar grid a search-result-artist

--- angular-app/src/app/search-result-artist/search-result-artist.component.ts
+++ angular-app/src/app/search-result-artist/search-result-artist.component.ts
@@ -14,10 +14,6 @@ export class SearchResultArtistComponent implements OnInit {
   searchResult: any = null;
   private subscription?: Subscription;
 
-  artistName: string = ''
-  followers: string = ''
-  genres: Array<string> = []
-
   constructor(private data: SearchCommService) {}
 
   ngOnInit(): void {
@@ -27,23 +23,14 @@ export class SearchResultArtistComponent implements OnInit {
     this.subscription = this.data.currentResult.pipe(
       filter(searchResult => searchResult !== null),
       tap(searchResult => {
-        //guardar nombre de artista
-        this.artistName = searchResult.artists.items[0].name
-        //guardar cantidad de followers
-        this.followers = searchResult.artists.items[0].followers.total
-        //guardar generos en array
-        for (let i = 0; i < searchResult.artists.items[0].genres.length; i++) {
-          
-          this.genres.push(searchResult.artists.items[0].genres[i]);
-          console.log("item lista:" + this.genres[i])
-        } 
+        
       })
     ).subscribe(searchResult => this.searchResult = searchResult);
     /*this.data.currentResult.subscribe(searchResult => this.searchResult = searchResult)*/
   }
 
   funcionTest(){
-    console.log("funcionTest()" + this.artistName)
+    console.log("funcionTest()")
   }
 
 
