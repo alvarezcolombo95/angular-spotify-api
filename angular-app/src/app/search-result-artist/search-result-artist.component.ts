ff1f52d2368fb406442bd4dbfc920f274eb8d701
Juan Martin Alvarez Colombo <85197820+alvarezcolombo95@users.noreply.github.com>
2023-11-05 12:24:53 -0300

    Mod search-result-artist para mostrar info en card

--- angular-app/src/app/search-result-artist/search-result-artist.component.ts
+++ angular-app/src/app/search-result-artist/search-result-artist.component.ts
@@ -15,6 +15,8 @@ export class SearchResultArtistComponent implements OnInit {
   private subscription?: Subscription;
 
   artistName: string = ''
+  followers: string = ''
+  genres: Array<string> = []
 
   constructor(private data: SearchCommService) {}
 
@@ -25,7 +27,16 @@ export class SearchResultArtistComponent implements OnInit {
     this.subscription = this.data.currentResult.pipe(
       filter(searchResult => searchResult !== null),
       tap(searchResult => {
+        //guardar nombre de artista
         this.artistName = searchResult.artists.items[0].name
+        //guardar cantidad de followers
+        this.followers = searchResult.artists.items[0].followers.total
+        //guardar generos en array
+        for (let i = 0; i < searchResult.artists.items[0].genres.length; i++) {
+          
+          this.genres.push(searchResult.artists.items[0].genres[i]);
+          console.log("item lista:" + this.genres[i])
+        } 
       })
     ).subscribe(searchResult => this.searchResult = searchResult);
     /*this.data.currentResult.subscribe(searchResult => this.searchResult = searchResult)*/
