c3924b799de7899ea55320f093d7f5d3605cabf0
JoelArraigada <arraigadajoel@gmail.com>
2023-11-05 17:45:41 -0300

    Diseno login page y conexion son spotify

--- angular-app/src/app/services/login.service.ts
+++ angular-app/src/app/services/login.service.ts
@@ -12,7 +12,7 @@ export class LoginService {
     const authEndPoint=`${SpotifyConfiguration.authEndpoint}?`;
     const clientId = `client_id=${SpotifyConfiguration.clientId}&`;
     const redirectUrl =`redirect_uri=${SpotifyConfiguration.redirectUrl}&`;
-    const scopes = `scopes=${SpotifyConfiguration.scope}&`;
+    const scopes = `scopes=${SpotifyConfiguration.scope.join('%20')}&`;
     const responseType= `response_type=token&show_dialog=true`;
     return authEndPoint + clientId + redirectUrl + scopes + responseType;
   }
