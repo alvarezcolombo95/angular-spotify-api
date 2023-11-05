4a3ed2d4cf463f9cd600ec85e0935203af5b9788
Juan Martin Alvarez Colombo <85197820+alvarezcolombo95@users.noreply.github.com>
2023-11-04 22:33:21 -0300

    Generar component search-result-artist

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { LogInComponent } from './log-in/log-in.component';
 import { SearchboxComponent } from './searchbox/searchbox.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SearchResultArtistComponent } from './search-result-artist/search-result-artist.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     NavbarComponent,
     AboutComponent,
     LogInComponent,
-    SearchboxComponent
+    SearchboxComponent,
+    SearchResultArtistComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-app/src/app/search-result-artist/search-result-artist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-result-artist',
+  templateUrl: './search-result-artist.component.html',
+  styleUrls: ['./search-result-artist.component.css']
+})
+export class SearchResultArtistComponent {
+
+}
