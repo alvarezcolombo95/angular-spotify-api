544e2aac2d88390cd6c5e9ffe278ef5c803e09da
Juan Martin Alvarez Colombo <85197820+alvarezcolombo95@users.noreply.github.com>
2023-11-11 11:41:20 -0300

    Arreglar parcialmente detail-artist

--- angular-app/src/app/detail-artist/detail-artist.component.ts
+++ angular-app/src/app/detail-artist/detail-artist.component.ts
@@ -1,4 +1,5 @@
-import { Component} from '@angular/core';
+import { Component, Input, SimpleChanges} from '@angular/core';
+import { SpotifySearchItemService } from '../services/spotify-search-item.service';
 
 @Component({
  selector: 'app-detail-artist',
@@ -7,5 +8,28 @@ import { Component} from '@angular/core';
 })
 export class DetailArtistComponent{
 
+
+    @Input() id: string = '';
+    artistResult: any = null;
+
+    constructor(private spotifyService: SpotifySearchItemService) {
+        
+    }
+
+    ngOnChanges(changes: SimpleChanges) {
+        if (changes['id']) {
+            this.doSomething(changes['id'].currentValue);
+        }
+    }
  
+    doSomething(value: string) {
+        this.callService()
+    }
+
+    async callService()
+    {
+        this.artistResult = await this.spotifyService.asyncCallGetArtist(this.id);
+        console.log("funcion test")
+        console.log(this.artistResult)
+    }
 }
