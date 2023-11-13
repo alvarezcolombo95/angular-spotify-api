2cd3c5e7b803e3486668332265eb9a5fd2bd9373
JoelArraigada <arraigadajoel@gmail.com>
2023-11-13 19:40:29 -0300

    crea playlist

--- angular-app/src/app/components/UserInfo/user-component/user-component.component.ts
+++ angular-app/src/app/components/UserInfo/user-component/user-component.component.ts
@@ -22,7 +22,7 @@ export class UserComponentComponent implements OnInit {
   term: string = 'medium_term';
   type: string = 'tracks';
 
-  constructor(private loginservice: LoginService, private userservice: UserService, private playlistService:PlaylistService) {
+  constructor(private loginservice: LoginService, private userservice: UserService, private playlistService: PlaylistService) {
   }
 
   async ngOnInit() {
@@ -33,39 +33,45 @@ export class UserComponentComponent implements OnInit {
     this.recentArtists = await this.getRecentItems('artists', 'medium_term');
   }
 
-    // this.recentItems = await this.getRecentItems('tracks', this.term);
 
-  getUris(){
-    let uriString:string = '';
-    if(this.recentItems && this.recentItems.length>0){
-      uriString = this.recentItems.map(track=>track.uri).join(',');
-    //   this.recentItems.forEach(track => {
-    //       uriString += track.uri.join(',');
-    // });
+  getUris() {//Devuelve un string de varias canciones
+    let uriString: string = '';
+    if (this.recentItems && this.recentItems.length > 0) {
+      uriString = this.recentItems.map(track => track.uri).join(',');
+      this.recentItems.forEach(track => {
+        uriString += track.uri.join(',');
+      });
     }
     console.log(uriString)
     return uriString;
-    
+
   }
-   async createPlaylist(){
+
+
+
+  async createPlaylist() {
     let token = this.loginservice.getToken()
     let userId = await this.getId();
-    if(token!=null)
-    {
-      //let playlistId = await this.playlistService.createPlaylist(token,userId,`${this.showType}+${this.termType}`)
-      let uriString = this.getUris();
-      console.log(uriString.split(',').map(uri => `"${uri}"`))
-      //this.playlistService.addTracks(playlistId,token,uriString.split(',').map(uri => `"${uri}"`));
-      //return playlistId;
+    let fechaActual = new Date();
+    let ano = fechaActual.getFullYear();
+    let mes = fechaActual.getMonth() + 1; 
+    let dia = fechaActual.getDate();
+    if (token != null) {
+      let playlistId = await this.playlistService.createPlaylist(token, userId, `${this.showType}-${this.termType}-${ano}-${mes}-${dia}`)
+      this.recentItems.forEach(track => {
+        if (token != null) {
+          this.playlistService.addTrack(playlistId, token, track.uri) //Agrego canciones 1 por 1
+        }
+      });
     }
-    }
+  }
 
 
   async toggleTermType() {
-    this.term = (this.term === 'short_term' ? 'medium_term' : 
-                    this.term === 'medium_term'? 'long_term':'short_term');
-    this.termType = this.termType === 'Ultimos 6 meses' ? 'All time' : 
-                    this.termType === 'All time'?'Ultimo mes':'Ultimos 6 meses';
+    this.term = (this.term === 'short_term' ? 'medium_term' :
+      this.term === 'medium_term' ? 'long_term' : 'short_term');
+    this.termType = this.termType === 'Ultimos 6 meses' ? 'All time' :
+      this.termType === 'All time' ? 'Ultimo mes' : 'Ultimos 6 meses';
     this.recentItems = await this.getRecentItems('tracks', this.term);
     this.recentArtists = await this.getRecentItems('artists', this.term);
   }
@@ -101,7 +107,7 @@ export class UserComponentComponent implements OnInit {
     }
   }
 
-  async getId(){
+  async getId() {
     try {
       const response = await this.userservice.getUser();
       const data = await response.json();
