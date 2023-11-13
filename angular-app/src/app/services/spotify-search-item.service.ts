be67527190c1a8433012b7d69fdd9808866de1ae
Juan Martin Alvarez Colombo <85197820+alvarezcolombo95@users.noreply.github.com>
2023-11-13 11:00:45 -0300

    Completar display detail-artist

--- angular-app/src/app/services/spotify-search-item.service.ts
+++ angular-app/src/app/services/spotify-search-item.service.ts
@@ -149,6 +149,43 @@ export class SpotifySearchItemService {
     
       });
     }
+
+    ///////////////////////////////////////////////////////////
     
+    async asyncCallGetRelatedArtists(input: string) {
+      console.log('Pidiendo');
+      try {
+        const res = await this.getRelatedArtists(input);
+        console.log('respuesta JSON:');
+        console.log(res);
+        return res;
+      } catch (error) {
+        console.log(error);
+        return error;
+      };
+    }
+    
+    getRelatedArtists(input: string) {
+      return new Promise(async (resolve, reject) => {
+        const data =null;
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://api.spotify.com/v1/artists/' + input + '/related-artists');
+        xhr.setRequestHeader('Authorization', `Bearer ${await this.GetToken()}`);
+    
+        xhr.onload = function () { 
+                if (xhr.status == 200) {
+                    console.log('respuesta del servidor o API Pedido Get Related Artists');
+                    let data = JSON.parse(this.response);
+                    resolve(data);
+                } else {
+                    reject(new Error('error en la conexion'));
+                }
+            };
+    
+            xhr.send(data); 
+    
+      });
+    }
+
 }
 
