fb52d5488d672d28ea9fdcd48727dd1ba8964ac7
Juan Martin Alvarez Colombo <85197820+alvarezcolombo95@users.noreply.github.com>
2023-11-19 05:17:08 -0300

    Agregar validación a búsqueda artista/cancion

--- angular-app/src/app/searchbox/searchbox.component.ts
+++ angular-app/src/app/searchbox/searchbox.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { SpotifySearchItemService } from '../services/spotify-search-item.service';
 import { AppRoutingModule } from '../app-routing.module';
 import { Router } from '@angular/router';
@@ -19,13 +19,13 @@ export class SearchboxComponent implements OnInit {
   
   constructor(private spotifyService: SpotifySearchItemService, private router: Router, private data: SearchCommService) {
     this.searchForm = new FormGroup({
-      searchStr: new FormControl('')
+      searchStr: new FormControl('', Validators.pattern(/^[a-zA-Z0-9]*$/))
     });
   }
 
   ngOnInit() {
     this.searchForm = new FormGroup({
-      searchStr: new FormControl('')
+      searchStr: new FormControl('', Validators.pattern(/^[a-zA-Z0-9]*$/))
     });
 
     this.data.currentResult.subscribe(searchResult => this.searchResult = searchResult)
@@ -39,23 +39,36 @@ export class SearchboxComponent implements OnInit {
 
   async searchMusic() {
 
-    this.router.navigate(['/loading-spinner'])
-    const searchStr = this.searchForm.get('searchStr')?.value;    
-
-    if(this.searchType == 'Artistas')
+    if(this.checkValiditySearch())
     {
-      this.router.navigate(['/search-result-artist'])
-      this.searchResult = await this.spotifyService.asyncCallSearchItem(searchStr, "artist");
-      
-    }
-    else if(this.searchType == 'Canciones')
-    {
-      this.router.navigate(['/search-result-song'])
-      this.searchResult = await this.spotifyService.asyncCallSearchItem(searchStr, "track");
-      
-    }
-
-    this.data.changeResult(this.searchResult);
-    
+      this.router.navigate(['/loading-spinner'])
+      const searchStr = this.searchForm.get('searchStr')?.value;    
+  
+      if(this.searchType == 'Artistas')
+      {
+        this.router.navigate(['/search-result-artist'])
+        this.searchResult = await this.spotifyService.asyncCallSearchItem(searchStr, "artist");
+        
+      }
+      else if(this.searchType == 'Canciones')
+      {
+        this.router.navigate(['/search-result-song'])
+        this.searchResult = await this.spotifyService.asyncCallSearchItem(searchStr, "track");
+        
+      }
+  
+      this.data.changeResult(this.searchResult);
+    }    
   }
+
+  checkValiditySearch() {
+    let validSearch = true;
+    if (this.searchForm.controls['searchStr'].invalid) {
+      alert('Por favor ingrese números o letras únicamente');
+      validSearch = false;
+    }
+
+    return validSearch;
+   }
+   
 }
