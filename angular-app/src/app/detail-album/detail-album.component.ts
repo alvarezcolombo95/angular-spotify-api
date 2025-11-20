e2eab1aee37341f046f04ef31889e74786d9bfc4
Juan Martin Alvarez Colombo <85197820+alvarezcolombo95@users.noreply.github.com>
2025-11-20 19:33:50 -0300

    Agregar features a Detail-album.component

--- angular-app/src/app/detail-album/detail-album.component.ts
+++ angular-app/src/app/detail-album/detail-album.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, Input, SimpleChanges} from '@angular/core';
+import { SpotifySearchItemService } from '../services/spotify-search-item.service';
 
 @Component({
-  selector: 'app-detail-album',
-  templateUrl: './detail-album.component.html',
-  styleUrls: ['./detail-album.component.css']
+ selector: 'app-detail-album',
+ templateUrl: './detail-album.component.html',
+ styleUrls: ['./detail-album.component.css']
 })
-export class DetailAlbumComponent {
+export class DetailAlbumComponent{
 
+
+    @Input() id: string = '';
+    albumResult: any = null;
+    
+
+    constructor(private spotifyService: SpotifySearchItemService) {}
+
+    ngOnChanges(changes: SimpleChanges) {
+        if (changes['id']) {
+            this.doSomething(changes['id'].currentValue);
+        }
+    }
+ 
+    doSomething(value: string) {
+        this.callService()
+    }
+
+    async callService()
+    {
+        this.albumResult = await this.spotifyService.asyncCallGetAlbum(this.id);
+    }
+
+    switchId(id: string)
+    {
+        this.id = id;
+        this.callService();
+    }
+
+    formatDuration(ms: number): string {
+        const totalSeconds = Math.floor(ms / 1000);
+        const minutes = Math.floor(totalSeconds / 60);
+        const seconds = totalSeconds % 60;
+        return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+    }
+
+    
 }
