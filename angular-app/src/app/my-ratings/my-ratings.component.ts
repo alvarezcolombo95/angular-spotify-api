d1afafcd2422e4ccaa03b5cde5f583e37517b852
Juan Martin Alvarez Colombo <85197820+alvarezcolombo95@users.noreply.github.com>
2025-11-21 13:22:58 -0300

    Crear my-ratings component

---
+++ angular-app/src/app/my-ratings/my-ratings.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-ratings',
+  templateUrl: './my-ratings.component.html',
+  styleUrls: ['./my-ratings.component.css']
+})
+export class MyRatingsComponent {
+
+}
